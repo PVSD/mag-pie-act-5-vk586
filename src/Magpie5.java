42d73cc94a441e786eac0305796e498f3f6aac55
Valentina Kononenko <valentina@kononenko.net>
2018-11-01 19:16:34 -0400

    Done

--- src/Magpie5.java
+++ src/Magpie5.java
@@ -284,7 +284,11 @@ public class Magpie5
 	private String [] randomResponses = {"Interesting, tell me more",
 			"Hmmm.",
 			"Do you really think so?",
-			"You don't say."
+			"You don't say.",
+			"Are you sure about that?",
+			"Mhmmm.",
+			"Oh cool!",
+			"I don't know what to tell ya!"
 	};
 	
 }
